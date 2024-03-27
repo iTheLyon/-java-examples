568be2876a50554f571f0747b5920dea78fb5b6c
jorge cotrado <jcotrado@senati.pe>
2024-03-27 17:58:07 -0500

    FIX Evaluar apuesta falta en caso ganaba segundo equipo

--- Tarea12/src/tarea12/ApuestasForm.java
+++ Tarea12/src/tarea12/ApuestasForm.java
@@ -25,6 +25,18 @@ public class ApuestasForm extends javax.swing.JFrame {
     private void initComponents() {
 
         jDialog1 = new javax.swing.JDialog();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTextArea1 = new javax.swing.JTextArea();
+        jButton2 = new javax.swing.JButton();
+        jDialog2 = new javax.swing.JDialog();
+        jLabel13 = new javax.swing.JLabel();
+        jLabel14 = new javax.swing.JLabel();
+        jLabel15 = new javax.swing.JLabel();
+        jTextField12 = new javax.swing.JTextField();
+        jTextField13 = new javax.swing.JTextField();
+        jButton3 = new javax.swing.JButton();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTextArea2 = new javax.swing.JTextArea();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
@@ -51,15 +63,127 @@ public class ApuestasForm extends javax.swing.JFrame {
         jTextField11 = new javax.swing.JTextField();
         jButton1 = new javax.swing.JButton();
 
+        jDialog1.setSize(new java.awt.Dimension(385, 210));
+
+        jTextArea1.setEditable(false);
+        jTextArea1.setColumns(20);
+        jTextArea1.setLineWrap(true);
+        jTextArea1.setRows(5);
+        jTextArea1.setBorder(null);
+        jTextArea1.setOpaque(false);
+        jScrollPane1.setViewportView(jTextArea1);
+
+        jButton2.setText("OK");
+
         javax.swing.GroupLayout jDialog1Layout = new javax.swing.GroupLayout(jDialog1.getContentPane());
         jDialog1.getContentPane().setLayout(jDialog1Layout);
         jDialog1Layout.setHorizontalGroup(
             jDialog1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jScrollPane1)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jDialog1Layout.createSequentialGroup()
+                .addContainerGap(146, Short.MAX_VALUE)
+                .addComponent(jButton2, javax.swing.GroupLayout.PREFERRED_SIZE, 94, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(145, 145, 145))
         );
         jDialog1Layout.setVerticalGroup(
             jDialog1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(jDialog1Layout.createSequentialGroup()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 162, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton2)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        jDialog2.setBackground(new java.awt.Color(255, 153, 102));
+        jDialog2.setBounds(new java.awt.Rectangle(0, 0, 862, 389));
+        jDialog2.setSize(new java.awt.Dimension(862, 389));
+
+        jLabel13.setFont(new java.awt.Font("Segoe UI", 1, 48)); // NOI18N
+        jLabel13.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel13.setText("SCORE DE PARTIDO");
+
+        jLabel14.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
+        jLabel14.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel14.setText("[nombre_primer_equipo]");
+
+        jLabel15.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
+        jLabel15.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel15.setText("[nombre_segundo_equipo]");
+
+        jTextField12.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
+        jTextField12.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jTextField12.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField12ActionPerformed(evt);
+            }
+        });
+
+        jTextField13.setFont(new java.awt.Font("Segoe UI", 0, 36)); // NOI18N
+        jTextField13.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jTextField13.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jTextField13ActionPerformed(evt);
+            }
+        });
+
+        jButton3.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        jButton3.setText("EVALUAR");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
+
+        jTextArea2.setColumns(20);
+        jTextArea2.setRows(5);
+        jScrollPane2.setViewportView(jTextArea2);
+
+        javax.swing.GroupLayout jDialog2Layout = new javax.swing.GroupLayout(jDialog2.getContentPane());
+        jDialog2.getContentPane().setLayout(jDialog2Layout);
+        jDialog2Layout.setHorizontalGroup(
+            jDialog2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jDialog2Layout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addComponent(jLabel14, javax.swing.GroupLayout.PREFERRED_SIZE, 367, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 46, Short.MAX_VALUE)
+                .addComponent(jLabel15, javax.swing.GroupLayout.PREFERRED_SIZE, 385, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(43, 43, 43))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jDialog2Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel13, javax.swing.GroupLayout.PREFERRED_SIZE, 594, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(120, 120, 120))
+            .addGroup(jDialog2Layout.createSequentialGroup()
+                .addGap(314, 314, 314)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 216, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jDialog2Layout.createSequentialGroup()
+                .addGap(140, 140, 140)
+                .addGroup(jDialog2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jScrollPane2)
+                    .addGroup(jDialog2Layout.createSequentialGroup()
+                        .addComponent(jTextField12, javax.swing.GroupLayout.PREFERRED_SIZE, 106, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jTextField13, javax.swing.GroupLayout.PREFERRED_SIZE, 107, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(158, 158, 158))
+        );
+        jDialog2Layout.setVerticalGroup(
+            jDialog2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jDialog2Layout.createSequentialGroup()
+                .addGap(12, 12, 12)
+                .addComponent(jLabel13)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jDialog2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel14)
+                    .addComponent(jLabel15))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jDialog2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jTextField12, javax.swing.GroupLayout.DEFAULT_SIZE, 86, Short.MAX_VALUE)
+                    .addComponent(jTextField13))
+                .addGap(7, 7, 7)
+                .addComponent(jButton3, javax.swing.GroupLayout.PREFERRED_SIZE, 72, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 138, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(19, Short.MAX_VALUE))
         );
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -192,25 +316,71 @@ public class ApuestasForm extends javax.swing.JFrame {
        String texto1 = jTextField1.getText();
        String mensaje ="";
        if(texto1.isEmpty()) {           
-           mensaje = mensaje + "Ingrese el nombre del primer Equipo.\r\n";           
+           mensaje = mensaje + "Ingrese el nombre del primer Equipo.\n";           
        }
        
        String texto2 = jTextField2.getText();
        if(texto2.isEmpty()) {           
-           mensaje = mensaje + " Ingrese el nombre del Segundo Equipo";           
+           mensaje = mensaje + "Ingrese el nombre del Segundo Equipo";           
        }       
        
        if(!mensaje.isEmpty()) {           
-        JLabel  jLabel1 = new JLabel();           
-        jLabel1.setText(mensaje);                   
-        jLabel1.setHorizontalAlignment(SwingConstants.CENTER);
-        jLabel1.setSize(350,100);           
-        jDialog1.setLocationRelativeTo(this);
-        jDialog1.setSize(400, 200);
-        jDialog1.add(jLabel1);                            
+        jTextArea1.setText(mensaje);
+        jDialog1.setLocationRelativeTo(this);       
         jDialog1.setVisible(true);           
        }
+       else {
+          String primerEquipo= jTextField1.getText();
+          String segundoEquipo= jTextField2.getText();
+          jLabel14.setText(primerEquipo);
+          jLabel15.setText(segundoEquipo);
+          jDialog2.setLocationRelativeTo(this);       
+          jDialog2.setVisible(true);
+       }
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jTextField12ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField12ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField12ActionPerformed
+
+    private void jTextField13ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextField13ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTextField13ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        // TODO add your handling code here:
+        //PARA EVALUAR LA APUESTA
+        String ScorePrimerEquipo = jTextField12.getText();
+        String ScoreSegundoEquipo = jTextField13.getText();
+        int scorePrimerEquipo = Integer.parseInt(ScorePrimerEquipo);
+        int scoreSegundoEquipo = Integer.parseInt(ScoreSegundoEquipo);
+        String resultado1="";
+        if(scorePrimerEquipo > scoreSegundoEquipo) {
+         resultado1 = "Ha ganado el equipo de "+jLabel14.getText()+"\n";
+         String cantidadApostadoresPrimerEquipo = jTextField3.getText();
+         String porcentajeGananciaPrimerEquipo = jTextField6.getText();
+         String montoTotalPrimerEquipo = jTextField9.getText();
+         double _porcentajeGananciaPrimerEquipo = Double.parseDouble(porcentajeGananciaPrimerEquipo) /100.00;
+         double _montoTotalPrimerEquipo = Double.parseDouble(montoTotalPrimerEquipo);
+         double gananciaPrimerEquipo = _porcentajeGananciaPrimerEquipo * _montoTotalPrimerEquipo;
+         resultado1 = resultado1 + "Todos los "+ cantidadApostadoresPrimerEquipo 
+                 +" apostadores han ganado " + "S/."+String.valueOf(gananciaPrimerEquipo)+ " Soles";
+        }        
+        else if (scorePrimerEquipo == scoreSegundoEquipo) {
+            resultado1 = "Ha empatado el equipo de "+jLabel14.getText()+"\n";
+            String cantidadApostadoresEmpate = jTextField4.getText();
+            String porcentajeGananciaEmpate = jTextField8.getText();
+            String montoTotalEmpate = jTextField11.getText();
+            double _porcentajeGananciaEmpate = Double.parseDouble(porcentajeGananciaEmpate) /100.00;
+            double _montoTotalEmpate = Double.parseDouble(montoTotalEmpate);
+            double gananciaEmpate = _porcentajeGananciaEmpate * _montoTotalEmpate;
+            resultado1 = resultado1 + "Todos los "+ cantidadApostadoresEmpate 
+                    +" apostadores ´por empate han ganado " + "S/."+String.valueOf(gananciaEmpate)+ " Soles";
+        }
+        
+        jTextArea2.setText(resultado1);
+        
+    }//GEN-LAST:event_jButton3ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -249,11 +419,17 @@ public class ApuestasForm extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
     private javax.swing.JDialog jDialog1;
+    private javax.swing.JDialog jDialog2;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel12;
+    private javax.swing.JLabel jLabel13;
+    private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
@@ -263,9 +439,15 @@ public class ApuestasForm extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JTextArea jTextArea2;
     private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField10;
     private javax.swing.JTextField jTextField11;
+    private javax.swing.JTextField jTextField12;
+    private javax.swing.JTextField jTextField13;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField4;
